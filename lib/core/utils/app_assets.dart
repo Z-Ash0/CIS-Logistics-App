82978f8227968301abe21685d42629bb8af65368
Z-Ash0 <zfire3005@gmail.com>
2025-07-08 04:18:47 +0300

    adding app assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,29 @@
+class Assets {
+  Assets._();
+
+  static const String assetsImagesCisLogo = "assets/images/cis_logo.svg";
+
+  static const String assetsImagesCisSplashScreen =
+      "assets/images/cis_splash_screen.png";
+
+  static const String assetsImagesModrekEmailCheck =
+      "assets/images/modrek_email_check.svg";
+
+  static const String assetsImagesModrekHomePage =
+      "assets/images/modrek_home_page.svg";
+
+  static const String assetsImagesModrekWithQr =
+      "assets/images/modrek_with_qr.svg";
+
+  static const String assetsImagesOnboardingBackgroung =
+      "assets/images/onboarding_backgroung.svg";
+
+  static const String assetsImagesPasswordCahngeSuccess =
+      "assets/images/password_cahnge_success.svg";
+
+  static const String assetsImagesSplashScreenDark =
+      "assets/images/splash_screen_dark.png";
+
+  static const String assetsImagesSplashScreenLight =
+      "assets/images/splash_screen_light.png";
+}
