7058a603840827eaf7b1b10b8853f846617bfa7f
Z-Ash0 <zfire3005@gmail.com>
2025-07-18 18:39:25 +0300

    Finishing user profile view and creating user text field

---
+++ lib/features/profile/presentation/widgets/user_text_field.dart
@@ -0,0 +1,94 @@
+import 'package:cis_logistics_app/core/helpers/extensions.dart';
+import 'package:cis_logistics_app/core/helpers/spacers.dart';
+import 'package:cis_logistics_app/core/utils/app_colors.dart';
+import 'package:cis_logistics_app/core/utils/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class UserTextField extends StatefulWidget {
+  const UserTextField({
+    super.key,
+    this.controller,
+    this.isPassword,
+    this.validator,
+    this.isReadOnly,
+    required this.labelText,
+  });
+
+  final TextEditingController? controller;
+  final bool? isPassword;
+  final bool? isReadOnly;
+  final String? Function(String?)? validator;
+  final String labelText;
+
+  @override
+  State<UserTextField> createState() => _UserTextFieldState();
+}
+
+class _UserTextFieldState extends State<UserTextField> {
+  bool _isObscured = true;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.isPassword == true) {
+      _isObscured = true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.labelText,
+          style: AppTextStyles.medium18.copyWith(color: AppColors.lightGreen),
+        ),
+        verticalSpace(8),
+        TextFormField(
+          controller: widget.controller,
+          validator: widget.validator,
+          autovalidateMode: AutovalidateMode.onUnfocus,
+          readOnly: widget.isReadOnly ?? false,
+          style: AppTextStyles.regular16.copyWith(color: AppColors.darkGreen),
+          obscureText: widget.isPassword == true ? _isObscured : false,
+          decoration: InputDecoration(
+            isDense: false,
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: context.setBasedOnScreenHeight(0.016),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none,
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: AppColors.lighterRed),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(color: AppColors.lighterRed),
+            ),
+            fillColor: AppColors.lightGray,
+            filled: true,
+            suffixIcon: widget.isPassword == true
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _isObscured = !_isObscured;
+                      });
+                    },
+                    icon: Icon(
+                      _isObscured ? Icons.visibility_off : Icons.visibility,
+                      color: AppColors.darkGray,
+                    ),
+                  )
+                : null,
+          ),
+          cursorColor: AppColors.darkGreen,
+        ),
+      ],
+    );
+  }
+}
